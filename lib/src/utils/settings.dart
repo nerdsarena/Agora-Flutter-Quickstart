de3d1d5bbba5a99844764fe3d858381071d0c013
Mohamed Fouad <devmfouad@gmail.com>
2021-12-01 19:16:00 +0200

    change expired temp token

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,3 @@
 /// Define App ID and Token
 const APP_ID = "aa0a9e404d0f4081910d0fc8e9fab181";
-const Token = "006aa0a9e404d0f4081910d0fc8e9fab181IABVl9dJ7J5egEDptmXdd7J+3jeVLhyd9MoFwytPkjci9xsLMHMAAAAAEACpCW2YYEWlYQEAAQBgRaVh";
+const Token = "006aa0a9e404d0f4081910d0fc8e9fab181IAB6focxxL4fZIFGJLwwwupn4uDgTtBmpXwtlLeSlgHa5xsLMHMAAAAAEADxtVSHpP2oYQEAAQCj/ahh";
