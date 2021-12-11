cc9cb97a0f8d3b15e665da05c69bbaed41058e1e
Mohamed Fouad <devmfouad@gmail.com>
2021-12-11 15:09:46 +0200

    new agora appid without temp token

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,3 +1,3 @@
 /// Define App ID and Token
-const APP_ID = "aa0a9e404d0f4081910d0fc8e9fab181";
-const Token = "006aa0a9e404d0f4081910d0fc8e9fab181IAB6focxxL4fZIFGJLwwwupn4uDgTtBmpXwtlLeSlgHa5xsLMHMAAAAAEADxtVSHpP2oYQEAAQCj/ahh";
+const APP_ID = "e68e99dd449a4604bcd959630225235b";
+const Token = "";
